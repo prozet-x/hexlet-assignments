93867100969da4f71357f334c9759efd76762312
@hexlet/cli <support@hexlet.io>
2023-02-22 02:20:19 +0700

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -1,5 +1,41 @@
 package exercise;
 
-// BEGIN
+import java.util.HashMap;
+import java.util.Map;
 
+// BEGIN
+public class FileKV implements KeyValueStorage {
+    private String filePath;
+
+    public FileKV(String filePath, Map<String, String> initianMap) {
+        this.filePath = filePath;
+    }
+
+    public void set(String key, String value) {
+        Map<String, String> map = readMapFromFile();
+        map.put(key, value);
+        writeMapToFile(map);
+    }
+
+    public String get(String key, String defaultValue) {
+        Map<String, String> map = readMapFromFile();
+        return map.getOrDefault(key, defaultValue);
+    }
+
+    public Map<String, String> toMap() { return new HashMap<String, String>(readMapFromFile()); }
+
+    public void unset(String key) {
+        Map<String, String> map = readMapFromFile();
+        map.remove(key);
+        writeMapToFile(map);
+    }
+
+    private void writeMapToFile(Map<String, String> map) {
+        Utils.writeFile(filePath, Utils.serialize(map));
+    }
+
+    private Map<String, String> readMapFromFile() {
+        return Utils.unserialize(Utils.readFile(filePath));
+    }
+}
 // END
--- java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
+++ java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
@@ -6,9 +6,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import com.fasterxml.jackson.databind.ObjectMapper;
-// BEGIN
+import java.util.Map;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+// BEGIN
+import static org.junit.jupiter.api.Assertions.*;
 // END
 
 
@@ -24,6 +27,24 @@ class FileKVTest {
     }
 
     // BEGIN
-    
+    @Test
+    public void testAll() {
+        final String defValue = "defValue";
+        FileKV fbd = new FileKV(filepath.toString(), Map.of());
+        assertEquals(fbd.toMap().isEmpty(), true);
+        fbd.set("key1", "value1");
+        assertEquals(fbd.toMap().size(), 1);
+        assertEquals(fbd.get("key1", defValue), "value1");
+        assertEquals(fbd.get("key2", defValue), defValue);
+        fbd.set("key2", "value2");
+        assertEquals(fbd.toMap().size(), 2);
+        assertEquals(fbd.get("key2", defValue), "value2");
+        fbd.set("key1", "value3");
+        assertEquals(fbd.toMap().size(), 2);
+        assertEquals(fbd.get("key1", defValue), "value3");
+        fbd.unset("key1");
+        assertEquals(fbd.toMap().size(), 1);
+        assertEquals(fbd.get("key1", defValue), defValue);
+    }
     // END
 }
