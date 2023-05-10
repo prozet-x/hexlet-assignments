5f1fdefe7132153c43c2c82aaeaa7a72ac245b6a
@hexlet/cli <support@hexlet.io>
2023-05-11 03:50:20 +0700

    submit java-spring-ru/filtration

---
+++ java-spring-ru/filtration/src/main/java/exercise/controller/UsersController.java
@@ -0,0 +1,30 @@
+package exercise.controller;
+import exercise.model.User;
+import exercise.model.QUser;
+import exercise.repository.UserRepository;
+import org.hibernate.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// Зависимости для самостоятельной работы
+// import org.springframework.data.querydsl.binding.QuerydslPredicate;
+// import com.querydsl.core.types.Predicate;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // BEGIN
+    @GetMapping
+    public Iterable<User> filterUsers(@RequestParam(name = "firstName", defaultValue = "") String firstName, @RequestParam(name = "lastName", defaultValue = "") String lastName) {
+        return userRepository.findAll(QUser.user.firstName.containsIgnoreCase(firstName).and(QUser.user.lastName.containsIgnoreCase(lastName)));
+    }
+    // END
+}
+
